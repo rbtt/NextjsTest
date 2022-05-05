aa8df30187d8c869ec0bf4a1eed4bfd33c0c98e9
rbtt <32610233+rbtt@users.noreply.github.com>
2022-05-05 15:04:44 +0300

    adding initial layout

--- sections/Main/Main.jsx
+++ sections/Main/Main.jsx
@@ -1,11 +1,33 @@
 import Image from "next/image";
 
-import { StyledContainer, StyledTitle } from "./elements";
+import {
+  StyledContainer,
+  StyledHeading,
+  StyledDescription,
+  StyledInnerContainer,
+  StyledBackgroundContainer,
+  StyledImageContainer,
+} from "./elements";
 
 export const Main = (props) => {
   return (
     <StyledContainer {...props}>
-      <StyledTitle>This is the Main Section</StyledTitle>
+      <StyledHeading>Managed agency selection</StyledHeading>
+      <StyledDescription>Strengthen your onboarding proccess</StyledDescription>
+
+      <StyledInnerContainer>
+        <StyledBackgroundContainer>
+          <Image
+            alt="background"
+            src="/img/background.png"
+            layout="fill"
+            objectFit="cover"
+          />
+        </StyledBackgroundContainer>
+        <StyledImageContainer>
+          <Image src="/img/video.png" width="329" height="525" alt="video" layout="" />
+        </StyledImageContainer>
+      </StyledInnerContainer>
     </StyledContainer>
   );
 };
--- sections/Main/elements.jsx
+++ sections/Main/elements.jsx
@@ -13,10 +13,37 @@ import {
 export const StyledContainer = styled(({ height, ...props }) => (
   <SectionContainer {...props} />
 ))`
+  display: flex;
   align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border: 1px solid red;
+  position: relative;
+  z-index: 1;
 `;
 
-export const StyledTitle = styled((props) => <SectionBigHeading {...props} />)`
+export const StyledHeading = styled((props) => <SectionHeading {...props} />)`
   margin: 0;
-  color: black;
+  text-align: center;
+`;
+
+export const StyledDescription = styled((props) => <SectionSubheading {...props} />)`
+  margin: 0;
+  margin-bottom: 3rem;
+`;
+
+export const StyledInnerContainer = styled((props) => <SectionContainer {...props} />)`
+  width: 100%;
+  height: 100%;
+  border: 2px solid green;
+`;
+
+export const StyledImageContainer = styled(({ ...props }) => <div {...props} />)``;
+
+export const StyledBackgroundContainer = styled(({ ...props }) => <div {...props}></div>)`
+  z-index: 0;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  border: 1px solid lightblue;
 `;
